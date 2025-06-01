5e0e9b22f537c3429e625de37f9612fbf1c28fba
ajnaselman <selmanajna@gmail.com>
2025-06-02 00:52:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navUL = document.querySelector('nav ul');
+
+  hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+  });
+});
 document.addEventListener("DOMContentLoaded", () => {
   const vozila = {
     Audi: {
